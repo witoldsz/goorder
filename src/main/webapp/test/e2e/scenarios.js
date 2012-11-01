25c4daa60bb41b0481531635e6ab8ba8494a8b7c
Łukasz Stachowiak <lukasz.stachowiak@espeo.pl>
2012-11-01 21:44:53 +0100

    Added e2e sample test and runner.

---
+++ src/main/webapp/test/e2e/scenarios.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('sample e2e', function() {
+
+  beforeEach(function() {
+    browser().navigateTo('../../index.html');
+  });
+  
+  it('sample test', function() {
+      
+    });
+  
+});
