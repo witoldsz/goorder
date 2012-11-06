df15cb11011db638d346ba49a4051145bae630e1
Witold Szczerba <pljosh.mail@gmail.com>
2012-11-06 01:30:01 +0100

    Testing WebSockets, using org.apache.catalina.websocket.WebSocketServlet.

---
+++ src/main/java/net/goorder/hellotest/TestWebSocket.java
@@ -0,0 +1,44 @@
+package net.goorder.hellotest;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.catalina.websocket.MessageInbound;
+import org.apache.catalina.websocket.StreamInbound;
+import org.apache.catalina.websocket.WebSocketServlet;
+
+/**
+ *
+ * @author witoldsz
+ */
+@WebServlet(urlPatterns="/hello/websocket")
+public class TestWebSocket extends WebSocketServlet {
+
+    @Override
+    protected StreamInbound createWebSocketInbound(String subProtocol, HttpServletRequest request) {
+        return new MessageInbound() {
+            {
+                System.out.println("Creating connection " + this.hashCode());
+                
+            }
+
+            @Override
+            protected void onBinaryMessage(ByteBuffer message) throws IOException {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            protected void onTextMessage(CharBuffer message) throws IOException {
+                CharBuffer cb = CharBuffer.wrap("Hello from " + getClass().getName());
+                getWsOutbound().writeTextMessage(cb);
+            }
+            @Override
+            protected void onClose(int status) {
+                System.out.println("Closing connection " + this.hashCode());
+            }
+        };
+    }
+    
+}
